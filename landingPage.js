1f84ba5e985c6cbfb786fb3842a450f58490ccdf
Amit Das <71481671+99002591@users.noreply.github.com>
2021-07-16 21:04:17 +0530

    Visual Upgrades

--- landingPage.js
+++ landingPage.js
@@ -2,7 +2,7 @@ var a = 0;
 function changeColor(color_id) {
    a = document.getElementById("colorOption");
    if(color_id === "glossBlack"){
-    a.src = "./resources/images/glossBlack.jpg"
+    a.src = "./resources/images/glossBlack.jpeg"
    }
    else if (color_id === "matteBlue"){
     a.src = "./resources/images/matteBlue.jpg"
